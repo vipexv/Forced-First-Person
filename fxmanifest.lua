4ae3f7e29955cce3f24fe52fd99c6f9cf0e4c7e7
vipex <101529155+vipexee@users.noreply.github.com>
2022-08-05 12:18:00 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,8 +2,9 @@ fx_version 'bodacious'
 game 'gta5'
 
 author 'vipex#2027'
-description 'Forced First Person When Attempting to Aim Ur Weapon Inside A Vehicle.'
+description 'Forced First Person with Multiple Options.'
 version '2.0'
+lua54 'yes'
 
 client_script 'client.lua'
 --server_script 'server.lua'
