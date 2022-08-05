b16c8a773d30d86fba02b5ad3f7c608f8db72667
vipex <101529155+vipexee@users.noreply.github.com>
2022-08-05 12:14:50 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,7 @@
+Config = {}
+
+
+Config.Options = {
+    VehicleOnly = false, -- Forced First Person when you are aiming inside of a vehicle only.
+    ForcedFirst = false, -- Forced First Person when the player is Aiming/Shooting.
+}
