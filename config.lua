634e37277ec3408250d1c9ddbf7ca31d6ae4aaff
vipex <101529155+vipexee@users.noreply.github.com>
2022-08-05 12:15:12 +0200

    Delete config.lua

--- config.lua
+++
@@ -1,7 +0,0 @@
-Config = {}
-
-
-Config.Options = {
-    VehicleOnly = false, -- Forced First Person when you are aiming inside of a vehicle only.
-    ForcedFirst = false, -- Forced First Person when the player is Aiming/Shooting.
-}
