67f33361b96f50ee1771f361cdad947433df4a55
vipex <101529155+vipexee@users.noreply.github.com>
2022-08-31 20:11:43 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,7 +3,7 @@ Config = {}
 Config.EnteringVehicle = false -- Forced First person if the player is driving a vehicle. (Only the Driver, not the other players.)
 
 Config.Options = {
-    VehicleOnly = false, -- Forced First Person when you are aiming inside of a vehicle only.
+    VehicleOnly = true, -- Forced First Person when you are aiming inside of a vehicle only.
     ForcedFirst = false, -- Forced First Person when the player is Aiming/Shooting.
     Bike = true, -- Enables forced first person when Aiming/Shooting On a Bike.
 }
