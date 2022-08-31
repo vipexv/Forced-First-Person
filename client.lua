b19029ba896c6088b1342c823aad7303a4996d71
vipex <101529155+vipexee@users.noreply.github.com>
2022-08-31 20:08:09 +0200

    Forced First Person When Aiming/Shooting On A Bike
    Forced First Person When Aiming/Shooting On A Bike Added.

--- client.lua
+++ client.lua
@@ -26,9 +26,10 @@ if Config.Options.VehicleOnly then
     CreateThread(function()
         while true do
             sleep = 1000
+            local bike = IsPedOnAnyBike(ped)
             local _, weapon = GetCurrentPedWeapon(PlayerPedId())
             local unarmed = `WEAPON_UNARMED`
-            if IsPedInAnyVehicle(PlayerPedId()) and weapon ~= unarmed then -- Message by vipex: Added an Extra check to lower resmon, if the player's weapon is Unarmed when inside of the Vehicle the Resmon will be 0.00.
+            if IsPedInAnyVehicle(ped) and weapon ~= unarmed then
                 sleep = 1
                 if IsPedDoingDriveby(PlayerPedId()) then
                     SetFollowVehicleCamViewMode(4)
@@ -42,6 +43,29 @@ if Config.Options.VehicleOnly then
         end
     end)
 end
+
+
+if Config.Options.Bike then
+    CreateThread(function()
+        while true do
+            sleep = 1000
+            local _, weapon = GetCurrentPedWeapon(PlayerPedId())
+            local unarmed = `WEAPON_UNARMED`
+            if IsPedOnAnyBike(PlayerPedId()) and weapon ~= unarmed then
+                sleep = 1
+                if IsControlJustPressed(0, 25) then
+                    SetCamViewModeForContext(2, 4)
+                elseif IsControlJustReleased(0, 25) then
+                    SetCamViewModeForContext(2, 0)
+                end
+            else
+                sleep = 1000
+            end
+            Wait(sleep)
+        end
+    end)
+end
+
 
 if Config.EnteringVehicle then
     CreateThread(function()
