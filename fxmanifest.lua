01fdf1f2ca897dc2ee93e8bb90792da74f5a203b
vipex <101529155+vipexv@users.noreply.github.com>
2023-01-05 21:56:02 +0100

    Script actually works now.
    Everything should be fully working and fixed.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,6 +5,6 @@ author 'vipex#2027'
 description 'Forced First Person with Multiple Options.'
 version '2.0'
 
-client_script 'client.lua'
+server_script 'main.lua'
 server_script 'config.lua'
 shared_script 'config.lua'
---
+++ main.lua
@@ -0,0 +1,64 @@
+if Config.Options.ForcedFirst then 
+    CreateThread(function()
+        while true do
+            sleep = 1000
+            local _, weapon = GetCurrentPedWeapon(PlayerPedId())
+            local unarmed = `WEAPON_UNARMED`
+            if weapon == unarmed then
+                sleep = 1000
+            else
+                sleep = 1
+                if IsPlayerFreeAiming(PlayerId()) then
+                    SetFollowPedCamViewMode(4)
+                else
+                    SetFollowPedCamViewMode(0)
+                end
+            end
+        Wait(sleep)
+        end
+    end)
+end
+
+
+if Config.Options.Vehicle then
+CreateThread(function()
+    while true do
+        local ped = PlayerPedId()
+        local _, weapon = GetCurrentPedWeapon(ped)
+        local unarmed = `WEAPON_UNARMED`
+        local inVeh = GetVehiclePedIsIn(PlayerPedId(), false)
+            sleep = 1000
+        if IsPedInAnyVehicle(PlayerPedId()) and weapon ~= unarmed then
+            sleep = 1
+            if IsPedDoingDriveby(PlayerPedId()) then
+                SetFollowVehicleCamViewMode(4)
+            else
+                SetFollowVehicleCamViewMode(0)
+                end
+            end
+            Wait(sleep)
+        end
+    end)
+end
+
+
+if Config.Options.Bike then
+    CreateThread(function()
+        while true do
+            sleep = 1000
+            local _, weapon = GetCurrentPedWeapon(PlayerPedId())
+            local unarmed = `WEAPON_UNARMED`
+            if IsPedOnAnyBike(PlayerPedId()) and weapon ~= unarmed then
+                sleep = 1
+                if IsControlJustPressed(0, 25) then
+                    SetCamViewModeForContext(2, 4)
+                elseif IsControlJustReleased(0, 25) then
+                    SetCamViewModeForContext(2, 0)
+                end
+            else
+                sleep = 1000
+            end
+            Wait(sleep)
+        end
+    end)
+end
